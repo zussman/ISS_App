2252af2deb070d5310a7cdc45026092038b2882f
David Zussman <david.zussman@gmail.com>
2015-06-14 07:46:00 -0400

    Fix Maps and View Formatting for ISS Data

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,15 +5,17 @@ class WelcomeController < ApplicationController
 
   def get_location
     iss_data = HTTParty.get("https://api.wheretheiss.at/v1/satellites/25544", :verify => false)
+    mi_per_km = 0.621371
     @name = iss_data["name"]
     @id = iss_data["id"]
     @latitude = iss_data["latitude"]
     @longitude = iss_data["longitude"]
-    @altitude = iss_data["altitude"]
-    @velocity = iss_data["velocity"]
+    @altitude = (iss_data["altitude"] * mi_per_km).round(2)
+    @velocity = (iss_data["velocity"] * mi_per_km).round(2)
     @visibility = iss_data["visibility"]
     @footprint = iss_data["footprint"]
-    @timestamp = iss_data["timestamp"].to_s
+    @timestamp = Time.at(iss_data["timestamp"].to_i)
+    @time = @timestamp.strftime('%H:%M:%S %p %Z')
     @daynum = iss_data["daynum"]
     @solar_lat = iss_data["solar_lat"]
     @solar_lon = iss_data["solar_lon"]
